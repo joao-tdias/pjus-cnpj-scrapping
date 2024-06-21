ef7142a59f20f3c3eaa6df62979a5c8794aad9eb
jptdias10 <jptdias10@gmail.com>
2024-06-21 01:48:16 -0300

    feat: import re

--- cnpj_scrapping.py
+++ cnpj_scrapping.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import re
 from selenium import webdriver
 from selenium.webdriver import Firefox
 from selenium.webdriver.common.by import By
@@ -55,7 +56,6 @@ def collect_data(driver):
 
 if __name__ == '__main__':
     path_cnpjs = 'input.csv'
-    cnpjs = pd.read_csv(path_cnpjs)    url_base = 'https://solucoes.receita.fazenda.gov.br/Servicos/cnpjreva/cnpjreva_solicitacao.asp'
     cnpjs = read_cnpjs(path_cnpjs)
 
     url_base = 'https://solucoes.receita.fazenda.gov.br/Servicos/cnpjreva/cnpjreva_solicitacao.asp'
